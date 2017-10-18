ebb75b69646633648b87581e038fb07af883a862
Alexander Nassian <picaschaf@me.com>
2017-10-18 08:55:53 +0200

    Initial simple implementation.

---
+++ main.cc
@@ -0,0 +1,53 @@
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
+
+#include <QtGui/QMouseEvent>
+#include <QtGui/QPainter>
+
+
+class PaintArea : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit PaintArea(QWidget* parent = nullptr) : QWidget(parent) {
+    }
+
+    virtual ~PaintArea() {
+    }
+
+protected:
+    void mouseMoveEvent(QMouseEvent* event) {
+        touchpoints.append(event->pos());
+        update();
+    }
+
+    void paintEvent(QPaintEvent* event) {
+        Q_UNUSED(event);
+
+        QPainter p(this);
+        p.fillRect(0, 0, width(), height(), Qt::white);
+        for (int i = 0; i < touchpoints.count(); i++) {
+            if (i + 1 >= touchpoints.count())
+                break;
+
+            p.drawLine(touchpoints.at(i), touchpoints.at(i + 1));
+        }
+    }
+
+private:
+    QVector<QPoint> touchpoints;
+};
+
+#include "main.moc"
+
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+
+    PaintArea window;
+    window.show();
+
+    return app.exec();
+}
