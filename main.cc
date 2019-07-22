6a3682e2a5b419ffcd42d7df22ad458d41250c53
Alexander Nassian <nassian@bitshift-dynamics.com>
2019-07-22 15:19:31 +0200

    Trivial reformatting.
    Signed-off-by: Alexander Nassian <nassian@bitshift-dynamics.com>

--- main.cc
+++ main.cc
@@ -5,18 +5,15 @@
 #include <QtGui/QPainter>
 
 
-class PaintArea : public QWidget
-{
+class PaintArea : public QWidget {
     Q_OBJECT
 
-public:
-    explicit PaintArea(QWidget* parent = nullptr) : QWidget(parent) {
+  public:
+    explicit PaintArea(QWidget* parent = nullptr)
+            : QWidget(parent) {
     }
 
-    virtual ~PaintArea() {
-    }
-
-protected:
+  protected:
     void mouseMoveEvent(QMouseEvent* event) {
         touchpoints.append(event->pos());
         update();
@@ -25,7 +22,7 @@ protected:
     void paintEvent(QPaintEvent* event) {
         Q_UNUSED(event);
 
-        QPainter p(this);
+        QPainter p { this };
         p.fillRect(0, 0, width(), height(), Qt::white);
         for (int i = 0; i < touchpoints.count(); i++) {
             if (i + 1 >= touchpoints.count())
@@ -40,18 +37,17 @@ protected:
             qApp->quit();
     }
 
-private:
+  private:
     QVector<QPoint> touchpoints;
 };
 
 #include "main.moc"
 
 
-int main(int argc, char** argv)
-{
-    QApplication app(argc, argv);
+int main(int argc, char** argv) {
+    QApplication app { argc, argv };
 
-    PaintArea window;
+    PaintArea window { nullptr };
     window.show();
 
     return app.exec();
